30a57bc88ed2c4cecb13eaf0c832d78c287163c2
Fathima28kousar <kousar28fathima@gmail.com>
2024-01-20 19:01:31 +0530

    test

--- react-12/src/App.js
+++ react-12/src/App.js
@@ -1,12 +1,64 @@
 import React from 'react'
-import User from './User'
-function App(){
-    return(
-        <div>
-            <h1>Hello</h1>
-            <User/>
-        </div>
-    )
+
+
+const Country = ({ country: { name, flag, population ,currency} }) => {
+  return (
+    <div className='country'>
+      <div className='country_flag'>
+        <img src={flag} alt={name} />
+      </div>
+      <h3 className='country_name'>{name.toUpperCase()}</h3>
+      <div class='country_text'>
+        <p>
+          <span>Population: </span>
+          {population}
+        </p>
+        <p>
+          <span>Currency: </span>
+          {currency}
+        </p>
+      </div>
+    </div>
+  )
 }
 
+class App extends React.Component {
+  state = {
+    data: [],
+  }
+
+  componentDidMount() {
+    this.fetchCountryData()
+  }
+
+  fetchCountryData = async () => {
+    const url = 'https://restcountries.eu/rest/v3/all'
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      this.setState({
+        data,
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Fetching API using Fetch</h1>
+        <h1>Calling API</h1>
+        <div>
+          <p>There are {this.state.data.length} countries in the api</p>
+          <div className='countries-wrapper'>
+            {this.state.data.map((country) => (
+              <Country country={country} />
+            ))}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
 export default App
