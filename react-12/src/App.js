745cf7d8c185cae704e7f27368cbcb5125727f5a
Fathima28kousar <kousar28fathima@gmail.com>
2024-01-24 21:50:56 +0530

    test

--- react-12/src/App.js
+++ react-12/src/App.js
@@ -1,14 +1,35 @@
 import React from 'react'
-import User from './User'
-import Product from './Product'
-class App extends React.Component{
+class App extends React.Component {
+    state =  {
+        uname:'',
+        email:'',
+        password: ''
+    }
+
+    emailHandler = (evt) => {
+        this.setState({
+            email: evt.target.value
+        })
+    }
+
+    passwordHandler = (evt) => {
+        this.setState({
+            password:evt.target.value
+        })
+    }
+
 
     render(){
         return(
-            <div>
-                <h1>App component!</h1>
-                <User/>
-                <Product/>
+            <div className='mt-5'>
+                <pre>{JSON.stringify(this.state)}</pre>
+                <form>
+                    <label>Email Id:</label>
+                    <input type='text' onChange={this.emailHandler}/> <br/>
+                    <label>Password</label>
+                    <input type='password' onChange={this.passwordHandler}/> <br/>
+                    <input type='submit' value='login'/>
+                </form>
             </div>
         )
     }
