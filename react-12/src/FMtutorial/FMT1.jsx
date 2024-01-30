91b750992e8713d061d2488a56205046f016eaff
Fathima28kousar <kousar28fathima@gmail.com>
2024-01-30 18:12:02 +0530

    framer motion tutorial

---
+++ react-12/src/FMtutorial/FMT1.jsx
@@ -0,0 +1,20 @@
+import {motion} from 'framer-motion'
+const FramerMotion = () => {
+  return (
+    <div>
+      <motion.h1
+        initial={{x: -100}}
+        animate = {{x:0}}
+    //    animate = {{x:[0,900,0]}}
+        transition = {{duration:2,
+            delay:0.2
+        }}
+        whileHover={{scale:0.9,opacity:0.2}}
+    >
+        This is animation
+      </motion.h1>
+    </div>
+  )
+}
+
+export default FramerMotion
