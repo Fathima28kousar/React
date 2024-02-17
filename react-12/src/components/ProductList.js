1f79c7c167e9b99945b1847b30f39693779172fe
Fathima28kousar <kousar28fathima@gmail.com>
2024-02-17 16:00:58 +0530

    test

--- react-12/src/components/ProductList.js
+++ react-12/src/components/ProductList.js
@@ -3,17 +3,22 @@ import { useSelector, useDispatch } from 'react-redux';
 import { addToCart ,} from '../redux/actions';
 import { useState } from 'react';
 
+
 const ProductList = () => {
   const products = useSelector((state) => state.products);
+  const [searchQuery, setSearchQuery] = useState('');
 //   console.log('rerendered',products)
   const dispatch = useDispatch();
   const maxProductPrice = useSelector((state) => state.maxProductPrice);
   const minProductPrice = Math.min(...products.map((product) => product.price));
   const [selectedProductPrice, setSelectedProductPrice] = useState(minProductPrice);
 
-  const filteredProducts = selectedProductPrice !== minProductPrice
-  ? products.filter((product) => product.price <= selectedProductPrice)
-  : products;
+  const filteredProducts = products.filter((product) => {
+    const matchesPrice = selectedProductPrice !== minProductPrice ? product.price <= selectedProductPrice : true;
+    const matchesSearch = product.name.toLowerCase().includes(searchQuery.toLowerCase());
+    return matchesPrice && matchesSearch;
+  });
+
 
   const handleAddToCart = (product) => {
     // console.log(product)
@@ -29,6 +34,14 @@ const ProductList = () => {
       <input type='range' min={minProductPrice} max={maxProductPrice} value={selectedProductPrice}
       onChange={(e) => setSelectedProductPrice(parseFloat(e.target.value))}/>
 
+<div>
+        <label>Search Product:</label>
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+      </div>
       <ul>
       {filteredProducts.map((product) => (
           <li key={product.id}>
